cf8f9f44df076bafac3bbca4a73970f2351728a8
hunterjreid <62681404+hunterjreid@users.noreply.github.com>
2023-07-10 11:10:00 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  ipcMain.on('open-animation', (event, animationName) => {
+    const animationWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+
+    animationWindow.loadFile('animation.html');
+    animationWindow.webContents.on('did-finish-load', () => {
+      animationWindow.webContents.send('animation-name', animationName);
+    });
+
+    animationWindow.on('closed', () => {
+      animationWindow = null;
+    });
+  });
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
